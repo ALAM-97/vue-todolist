60a39d7569eb0fef7a113f2951fe48e0dd8b7ad1
ALAM-97 <alexamara97@gmail.com>
2021-09-20 16:22:07 +0200

    Error

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 const app = new Vue({
     el: '#root',
     data: {
-        
+        todos: 
+        [
+            {
+                goal: "Fare i compiti",
+                done: false
+            },
+            {
+                goal: "Fare la spesa",
+                done: false
+            },
+            {
+                goal: "Fare il bucato",
+                done: false
+            }
+        ],
+        todoIndex: 0
     },
     methods: {
-
+        prova: function() {
+            console.log(this.todos[0].goal);
+        }
     },
 })
